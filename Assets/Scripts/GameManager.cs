88773d5229659d77e832fc808faaa7b985d9c31e
DanielK3005 <danielkaramzin@gmail.com>
2023-09-30 21:45:41 +0300

    Full Project Done

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using TMPro;
 
 
 public class GameManager : MonoBehaviour
@@ -12,6 +13,13 @@ public class GameManager : MonoBehaviour
 
     public int ghostMultiplier {get; private set; } = 1;
 
+    [SerializeField] TMP_Text gameOverText;
+    [SerializeField] TMP_Text scoreText;
+    [SerializeField] TMP_Text livesText;
+
+    [SerializeField] AudioSource gameOverAudio;
+
+    
     public int Score { get; private set; }
     public int Lives { get; private set; }
 
@@ -38,6 +46,8 @@ public class GameManager : MonoBehaviour
 
     public void NewRound()
     {
+        gameOverText.enabled = false;
+
         foreach(Transform pallet in pallets)
         {
             pallet.gameObject.SetActive(true);
@@ -57,15 +67,14 @@ public class GameManager : MonoBehaviour
 
     public void GameOver()
     {
-        foreach(Transform pallet in pallets)
-        {
-            pallet.gameObject.SetActive(false);
-        }
+        gameOverAudio.Play();
+        gameOverText.enabled = true;
 
         for(int i=0; i<ghosts.Length; i++)
         {
             ghosts[i].gameObject.SetActive(false);
         }
+
         pacman.gameObject.SetActive(false);
     }
 
@@ -73,14 +82,14 @@ public class GameManager : MonoBehaviour
     {
         int points = ghost.points * ghostMultiplier;
 
-        ghost.gameObject.SetActive(false);
         SetScore(Score + points);
         ghostMultiplier++;
     }
 
     public void PacmanEaten()
     {
-        pacman.gameObject.SetActive(false);
+        pacman.DeathSequence();
+        
         SetLives(Lives - 1);
 
         if(Lives > 0)
@@ -108,6 +117,11 @@ public class GameManager : MonoBehaviour
 
     public void PowerPalletEaten(PowerPallet powerPallet)
     {
+        for (int i = 0; i < ghosts.Length; i++) {
+            ghosts[i].frightened.Enable(powerPallet.duration);
+        }
+
+
         PalletEaten(powerPallet);
         CancelInvoke();
         Invoke(nameof(ResetGhostMultiplier), powerPallet.duration);
@@ -133,12 +147,14 @@ public class GameManager : MonoBehaviour
 
     private void SetScore(int score)
     {
-        Score= score;
+        Score = score;
+        scoreText.text = score.ToString().PadLeft(2, '0');
     }
 
     private void SetLives(int lives)
     {
-        Lives= lives;
+        Lives = lives;
+        livesText.text = "x" + lives.ToString();
     }
 
 }
